0be8a6e9740b71acda5a3e7d2bf0186ae22667f3
mattnewell <mnewell10@gmail.com>
2016-05-03 11:45:26 -0700

    Add simple content from controller to view using Jquery

---
+++ src/TravelBlog-AJAX/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using AjaxDemo.Models;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HelloAjax()
+        {
+            return Content("Hello from the controller!", "text/plain");
+        }
+    }
+}
---
+++ src/TravelBlog-AJAX/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxDemo.Models
+{
+    [Table("Persons")]
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Person(string email, string password, int id = 0)
+        {
+            Email = email;
+            Password = password;
+            Id = id;
+        }
+
+        public Person() { }  //whats this
+    }
+}
